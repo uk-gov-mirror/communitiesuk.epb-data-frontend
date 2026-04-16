fdc09e704e79539daf11038839f8011edc77db3d
Pedro Alvarez <pedro.alvarez@madetech.com>
2026-04-16 14:20:29 +0100

    Configure KMS client for development
    This change is to make it work in epb-dev-tools

--- lib/gateway/kms_gateway.rb
+++ lib/gateway/kms_gateway.rb
@@ -33,6 +33,15 @@ module Gateway
       case ENV["APP_ENV"]
       when "production"
         Aws::KMS::Client.new(region: "eu-west-2", credentials: Aws::ECSCredentials.new)
+      when "development"
+        Aws::KMS::Client.new(
+          region: "eu-west-2",
+          endpoint: ENV["AWS_KMS_ENDPOINT"],
+          credentials: Aws::Credentials.new(
+            ENV["AWS_ACCESS_KEY_ID"],
+            ENV["AWS_SECRET_ACCESS_KEY"],
+          ),
+        )
       else
         Aws::KMS::Client.new(stub_responses: true)
       end
